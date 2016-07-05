1fcaeec19c3c5986a92f041a7f8fb4c1b410eeed
Sven Kroll <Sven.Kroll@bbc.com>
2016-07-05 16:02:29 +0100

    add simple transaction

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,38 @@
 import static spark.Spark.*;
 
+import java.io.IOException;
+import java.io.StringWriter;
+
+import org.codehaus.jackson.map.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
 public class Main {
 
     public static void main(String[] args) {
+    	ObjectMapper mapper = new ObjectMapper();
     	Transaction transaction = createDummyTransaction();
         port(getHerokuAssignedPort());
         get("/hello", (req, res) -> "Hello Heroku World");
+        get("/transaction", (request, response) -> {
+            response.status(200);
+            response.type("application/json");
+            return dataToJson(transaction);
+        });
     }
 
-    static int getHerokuAssignedPort() {
+    private static Object dataToJson(Object data) {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            mapper.enable(SerializationFeature.INDENT_OUTPUT);
+            StringWriter sw = new StringWriter();
+            mapper.writeValue(sw, data);
+            return sw.toString();
+        } catch (IOException e){
+            throw new RuntimeException("IOException from a StringWriter?");
+        }
+	}
+
+	static int getHerokuAssignedPort() {
         ProcessBuilder processBuilder = new ProcessBuilder();
         if (processBuilder.environment().get("PORT") != null) {
             return Integer.parseInt(processBuilder.environment().get("PORT"));
