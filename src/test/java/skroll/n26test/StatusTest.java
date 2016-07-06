108547ba0904eb2e7db36f1605cd2d182a19cac1
Sven Kroell <Sven Kroell>
2016-07-06 23:21:44 +0200

    add status test

---
+++ src/test/java/skroll/n26test/StatusTest.java
@@ -0,0 +1,24 @@
+package skroll.n26test;
+
+import static org.junit.Assert.*;
+
+import org.json.simple.JSONObject;
+import org.junit.Before;
+import org.junit.Test;
+
+public class StatusTest {
+	private Status status;
+	
+	@Before
+	public void setUp() {
+		status = new Status();
+	}
+	
+	@Test
+	public void StatusOk() {
+		JSONObject jo = new JSONObject();
+		jo.put("Status", "OK");
+		assertEquals(jo, status.statusOK());
+	}
+
+}
