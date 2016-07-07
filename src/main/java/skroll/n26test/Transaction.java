83caaaf1c922c81837fa18f773654da86260de51
Sven Kroll <Sven.Kroll@bbc.com>
2016-07-07 14:02:27 +0100

    remove unused imports

--- src/main/java/skroll/n26test/Transaction.java
+++ src/main/java/skroll/n26test/Transaction.java
@@ -1,6 +1,5 @@
 package skroll.n26test;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.annotation.JsonProperty;
